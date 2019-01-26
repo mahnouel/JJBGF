f9dd3732b4b2ab48c4fab303273360c16b941072
Manuel <hello@m-k.io>
2019-01-26 19:29:02 +0100

    [FEAT] Update homepage and max-width for chrome

--- src/.vuepress/theme/layouts/Home.vue
+++ src/.vuepress/theme/layouts/Home.vue
@@ -2,19 +2,19 @@
   <div>
     <page-header/>
     <article class="mb-2 text">
-      <article
-        class="excerpt text"
+      <router-link
+        :to="page.path"
+        :key="page.key"
         v-for="page in $site.pages"
         v-if="page.frontmatter.featured"
-        :key="page.key"
+        class="excerpt"
       >
-        <h2>
-          {{page.frontmatter.title}}
+        <article class="text">
+          <h2>{{page.frontmatter.title}}</h2>
+          <p>{{page.frontmatter.excerpt}}</p>
           <wonder-time class="excerpt__time" :date="page.frontmatter.date"/>
-        </h2>
-        <p>{{page.frontmatter.excerpt}}</p>
-        <router-link :to="page.path" :key="page.key" class="button button--small">Weiterlesen</router-link>
-      </article>
+        </article>
+      </router-link>
       <Content class="text"/>
     </article>
     <page-footer/>
@@ -22,7 +22,21 @@
 </template>
 <style lang="stylus">
 .excerpt {
+  background-color: #ddd;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  display: block;
   margin-bottom: 2rem;
+  text-decoration: none;
+  color: inherit;
+
+  &:hover {
+    background-color: lighten(#ddd, 50%);
+
+    .excerpt__time {
+      background-color: lighten(#eee, 50%);
+    }
+  }
 
   h2 {
     margin-top: 0;
@@ -32,6 +46,10 @@
     background-color: #eee;
     color: #000;
     font-size: 1rem;
+  }
+
+  .excerpt__more {
+    display: inline-block;
     float: right;
   }
 }
