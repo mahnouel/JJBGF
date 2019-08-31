6717e86b0b11e5a9f63c32a8381d7322c05f74f7
Manuel <hello@m-k.io>
2019-08-31 10:54:17 +0200

    feat: remove kitawo from header

--- src/.vuepress/theme/global-components/PageHeader.vue
+++ src/.vuepress/theme/global-components/PageHeader.vue
@@ -5,7 +5,6 @@
     <nav class="page-header__nav">
       <router-link to="/" class="button">🏡 JJBGF</router-link>
       <router-link to="/events/" class="button">📅 Events</router-link>
-      <router-link to="/kitawo/" class="button">🕊 KiTaWo</router-link>
     </nav>
   </header>
 </template>
